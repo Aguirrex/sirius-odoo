6aa65b3d5da93e64f605f0a6e7a1c6ea5d88970b
vargasr03 <v.vargas@utp.edu.co>
2025-04-08 13:33:05 -0500

    Added securuty module

--- Facturacion/__manifest__.py
+++ Facturacion/__manifest__.py
@@ -1,19 +1,11 @@
 {
-    'name': 'Personalizacion Facturacion Sirius',
+    'name': 'Facturacion Personalizada',
     'version': '1.0',
-    'summary': 'Personalizaciones para el módulo de facturación',
-    'description': """
-        Este módulo personaliza el módulo de facturación de Odoo.
-        Agrega campos personalizados y modifica vistas.
-    """,
-    'author': 'Tu Nombre',
-    'website': 'http://www.tuweb.com',
-    'category': 'Account',
-    'depends': ['account'],
+    'depends': ['account'],  # <<--- clave: depende de 'account'
     'data': [
-        'views/account_move_views.xml',
+        'security/ir.model.access.csv',
+        'views/account.move.view.xml',
     ],
     'installable': True,
-    'application': False,
     'auto_install': False,
 }
--- Facturacion/models/account_move.py
+++ Facturacion/models/account_move.py
@@ -2,11 +2,4 @@ from odoo import models, fields, api
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
-
-    mi_campo_personalizado = fields.Char(string='Mi Campo Personalizado')
-
-    @api.model
-    def create(self, vals):
-        # Aquí puedes agregar lógica personalizada al crear una factura
-        res = super(AccountMove, self).create(vals)
-        return res
+    fact = fields.Char(string='Campo Extra')
