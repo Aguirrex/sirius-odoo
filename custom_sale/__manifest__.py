88598fe586ea54af98e4d247f21b40777d7555db
vargasr03 <v.vargas@utp.edu.co>
2025-04-08 16:23:59 -0500

    delete sale order actoins and menu

--- custom_sale/__manifest__.py
+++ custom_sale/__manifest__.py
@@ -3,8 +3,6 @@
     "version": "1.0",
     "depends": ["sale"],
     "data": [
-        "views/sale_order_actions.xml",
-        "views/sale_order_menu.xml",
         "views/sale_order_views.xml"
     ],
     "installable": True,
