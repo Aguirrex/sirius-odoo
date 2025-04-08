063fc846e65cbd483430e47d6b799399067d778f
Aguirrex <aguirrejuan670@gmail.com>
2025-04-08 13:21:10 -0500

    changed data flow

--- sales/models/custom_sale_order.py
+++ sales/models/custom_sale_order.py
@@ -5,7 +5,9 @@ from odoo import models, fields
 
 class CustomSaleOrder(models.Model):
     _inherit = 'sale.order'
-
+    _name = 'custom.sale.order'
+    _description = 'Custom Sale Order'
+    
     happy_user = fields.Boolean(
         string='Happy User',
         default=True)
