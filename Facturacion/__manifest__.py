01d537d9e6d52d6ba59e9e4541ea578a92c616da
vargasr03 <v.vargas@utp.edu.co>
2025-04-08 13:37:39 -0500

    Added securuty module

--- Facturacion/__manifest__.py
+++ Facturacion/__manifest__.py
@@ -2,10 +2,12 @@
     'name': 'Facturacion Personalizada',
     'version': '1.0',
     'depends': ['account'],  # <<--- clave: depende de 'account'
+    'depends': ['account'],
     'data': [
         'security/ir.model.access.csv',
         'views/account.move.view.xml',
     ],
+
     'installable': True,
     'auto_install': False,
 }
