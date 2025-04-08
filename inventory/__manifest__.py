5b534a79d1967e188933ed2f9475ffb1ae33cb51
Aguirrex <aguirrejuan670@gmail.com>
2025-04-08 18:48:16 -0500

    New trial

--- inventory/__manifest__.py
+++ inventory/__manifest__.py
@@ -7,10 +7,10 @@
     'category': 'Inventory',  # Categoría apropiada
     'depends': ['stock'],  # Dependencia del módulo stock
     'data': [
-        'views/stock_picking_view_inherit.xml',
+        'views/stock_picking_view_inherit.xml'
     ],
     'installable': True,
     'application': False,
     'auto_install': False,
-    "license": 'LGPL-3',
+    "license": 'LGPL-3'
 }
