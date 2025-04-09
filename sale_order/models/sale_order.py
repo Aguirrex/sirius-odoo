de123e96d55858be09b862b263e2e388e09cd3c0
vargasr03 <v.vargas@utp.edu.co>
2025-04-08 21:16:14 -0500

    sales module

--- sale_order/models/sale_order.py
+++ sale_order/models/sale_order.py
@@ -3,9 +3,9 @@ from odoo import models, fields
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    x_project_ref = fields.Char(string="Referencia de Proyecto")
     x_delivery_zone = fields.Selection([
         ('zona_norte', 'Zona Norte'),
         ('zona_sur', 'Zona Sur'),
         ('zona_centro', 'Zona Centro'),
     ], string="Zona de Entrega")
+
