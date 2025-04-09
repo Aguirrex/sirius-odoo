ff69869192541887e67bb7e4b530f7a097ad17b4
vargasr03 <v.vargas@utp.edu.co>
2025-04-09 03:16:19 -0500

    changes

--- sale_order/models/sale_order.py
+++ sale_order/models/sale_order.py
@@ -4,11 +4,18 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     x_delivery_zone = fields.Selection([
-    ('armenia_jardines', 'Armenia, Quindío - Calle 51 No. 13-06 Jardines'),
-    ('armenia_centro', 'Armenia, Quindío - Calle 21 No. 18-57 Centro'),
-    ('dosquebradas_badea', 'Dosquebradas, Risaralda - Calle 9 N° 5T - 120 La Badea'),
-    ('cali_centro', 'Cali, Valle del Cauca - Cra. 8 No 18 - 69 Local 101 Centro'),
-], string="Sucursal de Entrega")
-
+        ('sincelejo', 'Sincelejo'),
+        ('medellin', 'Medellín'),
+        ('manizales', 'Manizales'),
+        ('armenia', 'Armenia'),
+        ('ibague', 'Ibagué'),
+        ('dosquebradas', 'Dosquebradas'),
+        ('cali', 'Cali'),
+        ('popayan', 'Popayán'),
+        ('pasto', 'Pasto'),
+        ('florencia', 'Florencia'),
+        ('neiva', 'Neiva'),
+    ], string="Sucursal / Distribuidor de Entrega")
+    
     user_id = fields.Many2one('res.users', string='Salesperson')
 
