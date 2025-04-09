7cfb169997d063b1206917cab44423efd1e9b07b
vargasr03 <v.vargas@utp.edu.co>
2025-04-08 22:08:34 -0500

    changes sales

--- sale_order/__manifest__.py
+++ sale_order/__manifest__.py
@@ -8,7 +8,6 @@
     'data': [
         'security/ir.model.access.csv',
         'views/sale_order_views.xml',
-        'views/portal_quote_hide.xml',
     ],
     'installable': True,
     'application': False,
