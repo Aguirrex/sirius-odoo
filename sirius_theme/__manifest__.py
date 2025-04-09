12f4b7dea537d9c22620d8a780f2bd61aa814966
vargasr03 <v.vargas@utp.edu.co>
2025-04-08 22:27:06 -0500

    changes sales

---
+++ sirius_theme/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Sirius - Estilos Backend',
+    'version': '1.0',
+    'category': 'Tools',
+    'summary': 'Personalización visual del backend para Sirius',
+    'depends': ['web'],
+    'assets': {
+        'web.assets_backend': [
+            'sirius_theme/static/src/css/backend_styles.css',
+        ],
+    },
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
